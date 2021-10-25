28825e80e33c5efd2e7cd3054dc2ebcd7cd7ac3c
Adebolu Temitope <temitopeadebolu5@gmail.com>
2021-10-25 01:03:17 +0100

    Update DSN 400 Finalist to bootcamp Analysis.R

--- DSN 400 Finalist to bootcamp Analysis.R
+++ DSN 400 Finalist to bootcamp Analysis.R
@@ -1,47 +1,60 @@
+#This is an analysis of the DSN FINALISTS to the bootcap dataset
+
 #setwd("C:/Users/HP/Desktop/R")
 #DSN<-read.csv("dsn2021_bootcamp_finalist.CSV")
 DSN <- read.csv("https://drive.google.com/uc?export=download&id=1WscVgo79N-frRkFAg8_ufRiWZ_FWhGAr")
-print(DSN)
+#To see the first columns in the imported data
+head(DSN, n=10)
 
 # print number of columns
 print (ncol(DSN))
 
 # print number of rows
 print(nrow(DSN))
+
 #EDA
+
+
 summary(DSN)
 
 # list the variables in mydata
 names(DSN)
 # print first 10 rows of mydata
 head(DSN, n=10)
-#Frequency
 
+#Frequency of each variable
+#Frequency of the Gender of Finalists
 library("plyr")
 count(DSN,'Gender')
-#Visualization
+table(DSN$Gender)
+#Visualization of the Genderof Finalists
 library(ggplot2)
 ggplot(DSN,title = "Gender of Finalist", aes(x=reorder(Gender, Gender, function(x) - length(x)))) +
   geom_bar( width=0.4, fill="steelblue")+
   labs(x='Gender',y="Frequency")
+                                                       
 
-#AI-COMMUNITY EDA
-head(DSN$AI..Community, n=10)
-
+                                                       
+#AI-COMMUNITY                                          
+#Frequency of the AI-COMMUNITY
+count(DSN,'AI..Community')
+table(DSN$AI..Community)
 
 #View the top 10  Community that participated in the hackaton
 sort(table(DSN$AI..Community),decreasing=TRUE)[1:10]
 #changing the value "none" to "no commuinity"
-DSN$AI..Community[DSN$AI..Community=="None"]<-"No Community"
-
-
-#To visualize this using a Tree map..
-#creating any new column in the dataset representing the frequency of community
+"No Community"<-DSN$AI..Community[DSN$AI..Community=="None"]
+#To view the changes to the variable none
+head(DSN$AI..Community, n=10)
+                                                       
+#To visualize the AI-Community using a Tree map chart..
+                                                       
+library(treemap)                                                      
+#creating a new column in the dataset representing the frequency of community
 DSN$AI..Community <- as.character(DSN$AI..Community)
 DSN$countofcommunities <- as.numeric(ave(DSN$AI..Community, DSN$AI..Community, FUN = length))
 head(DSN, n=10)
 
-library(treemap)
 #creating a new column that would serve as a labelvalue on the tree map
 DSN$label <- paste(DSN$AI..Community, DSN$countofcommunities, sep = "\n")
 #plotting the tree map
@@ -52,22 +65,22 @@ treemap(DSN, #Your data frame object
         title = "Tree map to show the community with the most Finalists",#colors are determined by the index variables. Different branches in the hierarchical tree get different colors
         border.col ="white")
 
-#Visualization on a bar
+#To visualize the AI-Community on a bar-chart
 
 ggplot(DSN,title = "AI-COMMUNITIES", aes(x=reorder(AI..Community, AI..Community, function(x) - length(x)))) +
   geom_bar( width=0.4, fill="BROWN")+
   labs(x='AI..Community',y="Frequency")+theme(axis.text.x = element_text(angle = 90))
 
-#Creating a new column to indicate type of community
+                                                   
+#Type of community                                                   
+#Creating a new column to indicate type of community byusing the first word from every AI-COMMUNITY
 library(stringr)
-
+                                                 
 DSN$Type.of.community <- word(DSN$AI..Community, 1)
-
+#View the first10 values of the new column signifying type of community
 head(DSN$Type.of.community, n=10)
 
-
-#Visualization with Pie-Chart
-
+#Visualization of type of community with Pie-Chart
 library(tidyverse)
 DSN %>%
   count(Type.of.community) %>% 
@@ -84,6 +97,7 @@ DSN %>%
              fill="#69b3a2")
 
 theme_void()
-
+#Given the limited dataset with just 3 useful informations, these are the only Analysis thatcan be drawn from the dataset
+#I am Adebolu,thank you for being a part of my journey and progress                                                   
 
 
